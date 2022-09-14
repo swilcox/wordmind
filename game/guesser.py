3592952464bb72db605629cf58eca3c0e7067c8b
swilcox <steven@wilcoxzone.com>
2022-09-14 01:25:46 -0500

    add simplistic dict cache support

--- game/guesser.py
+++ game/guesser.py
@@ -6,11 +6,14 @@ from game.engine import Game, GameStatus, HintType
 
 
 class Guesser:
-    def __init__(self, game: Game, word_list: list[str], starting_word: str = ""):
+    def __init__(self, game: Game, word_list: list[str], starting_word: str = "", search_cache: dict = None):
         self.word_list = word_list
         self.starting_word = (
             starting_word if starting_word else random.choice(word_list)
         )
+        self._use_cache = True if search_cache is not None else False
+        self.cache = search_cache
+            
         self.game = game
 
     def compute_guess(self) -> str:
@@ -28,13 +31,27 @@ class Guesser:
                 for hint in self.game.hints:
                     if hint.hint_letters[i].hint_type == HintType.WRONG_SPOT:
                         addtl_excluded += hint.hint_letters[i].letter
-                regex += f"[^{exclude_letters+addtl_excluded}]"
+                if len(exclude_letters+addtl_excluded):
+                    regex += f"[^{exclude_letters+addtl_excluded}]"
+                else:
+                    regex += f"[a-z]"
                 if hl.hint_type == HintType.WRONG_SPOT:
                     must_contain.append(hl.letter)
-        r = re.compile(regex)
+        try:
+            r = re.compile(regex)
+        except Exception as ex:
+            print("offending regex: " + regex)
+            raise ex
+        if self._use_cache:
+            tmp_words = self.cache.get(regex, None) or list(filter(r.match, self.word_list))
+            if regex not in self.cache:
+                self.cache[regex] = tmp_words
+            #print(len(tmp_words))
+        else:
+            tmp_words = filter(r.match, self.word_list)
         possible_words = [
             pw
-            for pw in filter(r.match, self.word_list)
+            for pw in tmp_words
             if all(must_contain.count(l) <= pw.count(l) for l in must_contain)
         ]
         return random.choice(possible_words)
