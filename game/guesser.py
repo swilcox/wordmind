c45b7f6827b875a1f7a1b6ba1e5f30bb87d30c77
swilcox <swilcox@users.noreply.github.com>
2022-09-14 06:27:11 +0000

    :art: Format Python code with psf/black

--- game/guesser.py
+++ game/guesser.py
@@ -6,14 +6,20 @@ from game.engine import Game, GameStatus, HintType
 
 
 class Guesser:
-    def __init__(self, game: Game, word_list: list[str], starting_word: str = "", search_cache: dict = None):
+    def __init__(
+        self,
+        game: Game,
+        word_list: list[str],
+        starting_word: str = "",
+        search_cache: dict = None,
+    ):
         self.word_list = word_list
         self.starting_word = (
             starting_word if starting_word else random.choice(word_list)
         )
         self._use_cache = True if search_cache is not None else False
         self.cache = search_cache
-            
+
         self.game = game
 
     def compute_guess(self) -> str:
@@ -31,7 +37,7 @@ class Guesser:
                 for hint in self.game.hints:
                     if hint.hint_letters[i].hint_type == HintType.WRONG_SPOT:
                         addtl_excluded += hint.hint_letters[i].letter
-                if len(exclude_letters+addtl_excluded):
+                if len(exclude_letters + addtl_excluded):
                     regex += f"[^{exclude_letters+addtl_excluded}]"
                 else:
                     regex += f"[a-z]"
@@ -43,10 +49,12 @@ class Guesser:
             print("offending regex: " + regex)
             raise ex
         if self._use_cache:
-            tmp_words = self.cache.get(regex, None) or list(filter(r.match, self.word_list))
+            tmp_words = self.cache.get(regex, None) or list(
+                filter(r.match, self.word_list)
+            )
             if regex not in self.cache:
                 self.cache[regex] = tmp_words
-            #print(len(tmp_words))
+            # print(len(tmp_words))
         else:
             tmp_words = filter(r.match, self.word_list)
         possible_words = [
